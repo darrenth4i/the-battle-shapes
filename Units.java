6ff8bb6c8c0832f88e6af84c4bd7d7bc579da153
JustYe <341153946@gapps.yrdsb.ca>
2024-10-31 10:48:58 -0400

    abstracted

--- Units.java
+++ Units.java
@@ -3,17 +3,30 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Units here.
  * 
- * @author (your name) 
+ * @author (Justin Ye and Andy Li) 
  * @version (a version number or a date)
  */
-public class Units extends SuperSmoothMover
+public abstract class Units extends SuperSmoothMover
 {
     /**
      * Act - do whatever the Units wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    protected int speed;
+    protected int atk;
+    protected int health;
+    protected boolean frontIsClear;
+    
     public void act()
     {
-        // Add your action code here.
+        
+    }
+    
+    public void walk(int direction)
+    {
+        if(frontIsClear)
+        {
+            move(direction*speed);
+        }
     }
 }
