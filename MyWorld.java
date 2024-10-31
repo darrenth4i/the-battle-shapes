063df052b674b490e97452e3446259978a8d4ea1
Luscieu <150149720+Luscieu@users.noreply.github.com>
2024-10-31 10:28:52 -0400

    test

--- MyWorld.java
+++ MyWorld.java
@@ -18,4 +18,8 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1024, 800, 1); 
     }
+    public void randomMethod()
+    {
+        return;
+    }
 }
