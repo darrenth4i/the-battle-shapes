116c4708ed624816c8e36cecda3f555e4063f64b
Luscieu <150149720+Luscieu@users.noreply.github.com>
2024-10-31 10:45:57 -0400

    added background

--- MyWorld.java
+++ MyWorld.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    private GreenfootImage bg = new GreenfootImage("Backgrounds/battlecatsbg.png");
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -17,5 +17,7 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1024, 800, 1); 
+        bg.scale(1024,800);
+        setBackground(bg);
     }
 }
