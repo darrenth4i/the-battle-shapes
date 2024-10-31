a3a7920ffd459378e40f6c9c6faceff1e8ebe175
JustYe <341153946@gapps.yrdsb.ca>
2024-10-31 11:18:53 -0400

    Update Units.java

--- Units.java
+++ Units.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Units here.
  * 
- * @author (your name) 
+ * @author (Justin Ye and Andy Li) 
  * @version (a version number or a date)
  */
 public abstract class Units extends SuperSmoothMover
