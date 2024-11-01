540a09d20895b7ebbf1466595a27a14f073aa4a5
And E <348726067@gapps.yrdsb.ca>
2024-11-01 10:41:45 -0400

    Square debug code removed

--- Square.java
+++ Square.java
@@ -37,12 +37,7 @@ public abstract class Square extends Units
         Circle target = getObjectsInRange(getImage().getWidth()+30, Circle.class).size() != 0 ? getObjectsInRange(getImage().getWidth()+30, Circle.class).get(0) : null;
         if(target != null)
         {
-            System.out.println("hit");
             target.hurt(atk);
-        }
-        else
-        {
-            System.out.println("miss");
         }
     }
     
