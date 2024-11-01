06fda98579eb4146624c09df2575d47d1903295d
And E <348726067@gapps.yrdsb.ca>
2024-11-01 13:58:58 -0400

    Fixed unit

--- Circle.java
+++ Circle.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public abstract class Circle extends Units
+public abstract class Circle extends Unit
 {
     /**
      * Act - do whatever the Circle wants to do. This method is called whenever
--- Square.java
+++ Square.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public abstract class Square extends Units
+public abstract class Square extends Unit
 {
     /**
      * Act - do whatever the Square wants to do. This method is called whenever
