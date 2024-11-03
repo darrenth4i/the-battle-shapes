01a77b7d20863ad4d97e7b8ef4c55a64cead4077
JustYe <341153946@gapps.yrdsb.ca>
2024-11-02 22:47:56 -0400

    Added CTank and fixed Buttons

--- MyWorld.java
+++ MyWorld.java
@@ -28,9 +28,15 @@ public class MyWorld extends World
         
         addObject(new SpawnUnitButton("SFodder", 1, 0, 3000), 100, 80);
         addObject(new SpawnUnitButton("SWarrior", 1, 0, 3000), 200, 80);
+        addObject(new SpawnUnitButton("STank", 1, 0, 3000), 300, 80);
+        
         
         addObject(new SpawnUnitButton("CFodder", 1, 0, 3000), 800, 80);
+        addObject(new SpawnUnitButton("CFodder", 2, 0, 3000), 800, 180);
+        addObject(new SpawnUnitButton("CFodder", 3, 0, 3000), 800, 280);
         addObject(new SpawnUnitButton("CWarrior", 1, 0, 3000), 900, 80);
+        addObject(new SpawnUnitButton("CWarrior", 2, 0, 3000), 900, 180);
+        addObject(new SpawnUnitButton("CWarrior", 3, 0, 3000), 900, 280);
         
         //Cursor shows up on top of everything
         setPaintOrder(Cursor.class);
