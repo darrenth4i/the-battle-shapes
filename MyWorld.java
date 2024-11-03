cb75884f5e36ccc3de16f9d64783f209c6c052b0
Braoden <bradennchan@gmail.com>
2024-11-03 00:13:06 -0400

    updated button images

--- MyWorld.java
+++ MyWorld.java
@@ -29,16 +29,19 @@ public class MyWorld extends World
         addObject(new CTank(2), 900, 500);
         
         addObject(new SpawnUnitButton("SFodder", 1, 0, 3000), 100, 80);
-        addObject(new SpawnUnitButton("SWarrior", 1, 0, 3000), 200, 80);
-        addObject(new SpawnUnitButton("STank", 1, 0, 3000), 300, 80);
+        addObject(new SpawnUnitButton("SWarrior", 1, 0, 3000), 300, 80);
+        addObject(new SpawnUnitButton("STank", 1, 0, 3000), 200, 80);
         
         
-        addObject(new SpawnUnitButton("CFodder", 1, 0, 3000), 800, 80);
-        addObject(new SpawnUnitButton("CFodder", 2, 0, 3000), 800, 180);
-        addObject(new SpawnUnitButton("CFodder", 3, 0, 3000), 800, 280);
-        addObject(new SpawnUnitButton("CWarrior", 1, 0, 3000), 900, 80);
-        addObject(new SpawnUnitButton("CWarrior", 2, 0, 3000), 900, 180);
-        addObject(new SpawnUnitButton("CWarrior", 3, 0, 3000), 900, 280);
+        addObject(new SpawnUnitButton("CFodder", 1, 0, 3000), 924, 80);
+        addObject(new SpawnUnitButton("CFodder", 2, 0, 3000), 924, 180);
+        addObject(new SpawnUnitButton("CFodder", 3, 0, 3000), 924, 280);
+        addObject(new SpawnUnitButton("CTank", 1, 0, 3000), 824, 80);
+        addObject(new SpawnUnitButton("CTank", 2, 0, 3000), 824, 180);
+        addObject(new SpawnUnitButton("CTank", 3, 0, 3000), 824, 280);
+        addObject(new SpawnUnitButton("CWarrior", 1, 0, 3000), 724, 80);
+        addObject(new SpawnUnitButton("CWarrior", 2, 0, 3000), 724, 180);
+        addObject(new SpawnUnitButton("CWarrior", 3, 0, 3000), 724, 280);
         
         //Cursor shows up on top of everything
         setPaintOrder(Cursor.class);
