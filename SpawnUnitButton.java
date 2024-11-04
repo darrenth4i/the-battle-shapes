e315157bfad6d896211ebaea28ce20b5321cce3d
darrenth4i <150176044+darrenth4i@users.noreply.github.com>
2024-11-04 14:40:03 -0500

    small optimization to spawnunitbutton

--- SpawnUnitButton.java
+++ SpawnUnitButton.java
@@ -14,6 +14,8 @@ public class SpawnUnitButton extends UI
     private BlackBox hoverBox;
     private CooldownBar cooldownBar;
     
+    private ArrayList<SpawnUnitButton> buttons;
+
     //Boolean to determine if the cursor code "clicks" button 
     private boolean clicked;
 
@@ -23,7 +25,7 @@ public class SpawnUnitButton extends UI
         unitStage = stage;
         //cooldown is in milliseconds
         unitcooldown = cooldown;
-        if (u.equals("CFodder") || u.equals("CTank") || u.equals("CRanger") || u.equals("CHealer") || u.equals("CWarrior")) {
+        if (u.substring(0, 1).equals("C")) {
             circle = true;
         } else {
             circle = false;
@@ -38,7 +40,7 @@ public class SpawnUnitButton extends UI
         clicked = false;
         spawned = true;
     }
-
+    
     public void act() {
         if (spawned) {
             ArrayList<Tower> towers = (ArrayList<Tower>)getWorld().getObjects(Tower.class);
@@ -51,7 +53,7 @@ public class SpawnUnitButton extends UI
             }
 
             getWorld().addObject(new Text("$" + unitCost, 20), getX() - getImage().getWidth()/2 + 21, getY() + getImage().getHeight()/2 - 13);
-
+            
             spawned = false;
         }
         if (onCooldown && cooldownTimes >= 50) {
@@ -85,19 +87,38 @@ public class SpawnUnitButton extends UI
     }
 
     public void spawnUnit() {
-        if (unit == "CFodder") {getWorld().addObject(new CFodder(unitStage), spawn.getX(), spawn.getY() + 70 + Greenfoot.getRandomNumber(30));
-        } else if (unit == "CTank") {getWorld().addObject(new CTank(unitStage), spawn.getX(), spawn.getY() + 70 + Greenfoot.getRandomNumber(30));
-        } else if (unit == "CRanger") {getWorld().addObject(new CRanger(unitStage), spawn.getX(), spawn.getY() + 70 + Greenfoot.getRandomNumber(30));
-        } else if (unit == "CHealer") {getWorld().addObject(new CHealer(unitStage), spawn.getX(), spawn.getY() + 70 + Greenfoot.getRandomNumber(30));
-        } else if (unit == "CWarrior") {getWorld().addObject(new CWarrior(unitStage), spawn.getX(), spawn.getY() + 70 + Greenfoot.getRandomNumber(30));
-        } else if (unit == "SFodder") {getWorld().addObject(new SFodder(unitStage), spawn.getX(), spawn.getY() + 70 + Greenfoot.getRandomNumber(30));
-        } else if (unit == "STank") {getWorld().addObject(new STank(unitStage), spawn.getX(), spawn.getY() + 40 + Greenfoot.getRandomNumber(30));
-        } else if (unit == "SRanger") {getWorld().addObject(new SRanger(unitStage), spawn.getX(), spawn.getY() + 70 + Greenfoot.getRandomNumber(30));
-        } else if (unit == "SHealer") {getWorld().addObject(new SHealer(unitStage), spawn.getX(), spawn.getY() + 70 + Greenfoot.getRandomNumber(30));
-        } else if (unit == "SWarrior") {getWorld().addObject(new SWarrior(unitStage), spawn.getX(), spawn.getY() + 70 + Greenfoot.getRandomNumber(30));
+        if (unit == "CFodder") {
+            getWorld().addObject(new CFodder(unitStage), spawn.getX(), spawn.getY() + 70 + Greenfoot.getRandomNumber(30));
+        } 
+        else if (unit == "CTank") {
+            getWorld().addObject(new CTank(unitStage), spawn.getX(), spawn.getY() + 70 + Greenfoot.getRandomNumber(30));
+        } 
+        else if (unit == "CRanger") {
+            getWorld().addObject(new CRanger(unitStage), spawn.getX(), spawn.getY() + 70 + Greenfoot.getRandomNumber(30));
+        } 
+        else if (unit == "CHealer") {
+            getWorld().addObject(new CHealer(unitStage), spawn.getX(), spawn.getY() + 70 + Greenfoot.getRandomNumber(30));
+        } 
+        else if (unit == "CWarrior") {
+            getWorld().addObject(new CWarrior(unitStage), spawn.getX(), spawn.getY() + 70 + Greenfoot.getRandomNumber(30));
+        } 
+        else if (unit == "SFodder") {
+            getWorld().addObject(new SFodder(unitStage), spawn.getX(), spawn.getY() + 70 + Greenfoot.getRandomNumber(30));
+        } 
+        else if (unit == "STank") {
+            getWorld().addObject(new STank(unitStage), spawn.getX(), spawn.getY() + 40 + Greenfoot.getRandomNumber(30));
+        } 
+        else if (unit == "SRanger") {
+            getWorld().addObject(new SRanger(unitStage), spawn.getX(), spawn.getY() + 70 + Greenfoot.getRandomNumber(30));
+        } 
+        else if (unit == "SHealer") {
+            getWorld().addObject(new SHealer(unitStage), spawn.getX(), spawn.getY() + 70 + Greenfoot.getRandomNumber(30));
+        } 
+        else if (unit == "SWarrior") {
+            getWorld().addObject(new SWarrior(unitStage), spawn.getX(), spawn.getY() + 70 + Greenfoot.getRandomNumber(30));
         }
     }
-    
+
     /**
      * Method to darken the button upon cursor hover
      */
@@ -122,14 +143,14 @@ public class SpawnUnitButton extends UI
             getWorld().removeObject(hoverBox);
         }
     }
-    
+
     /**
      * Setter method to change clicked 
      */
     public void setClicked(boolean c){
         clicked = c;   
     }
-    
+
     /**
      * Getter method to return onCooldown
      */
