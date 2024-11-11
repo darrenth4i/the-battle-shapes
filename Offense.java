fffb58ec6ec972a770fd88d8460288fb83ede1cb
And E <348726067@gapps.yrdsb.ca>
2024-11-10 20:56:54 -0500

    Added new constructor for world
    Added constructor that doesn't take any parameters for debugging purposes. All units are the basic ones by default

--- Offense.java
+++ Offense.java
@@ -24,7 +24,7 @@ public class Offense extends Tower
     {
         if(getNearestOppositeShape() != null)
         {
-            getWorld().addObject(new OffenseProjectile(circle,getNearestOppositeShape(),1, 1), getX(), getY()-getImage().getHeight()/4);
+            getWorld().addObject(new OffenseProjectile(circle,getNearestOppositeShape(),1, 1), getX(), getY());
         }
     }
 }
--- SimulationWorld.java
+++ SimulationWorld.java
@@ -67,8 +67,54 @@ public class SimulationWorld extends World
         addObject(new SpawnUnitButton(cU1, 0, 1, 3000, true, true, true), 900, 605);
         addObject(new SpawnUnitButton(cU2, 1, 1, 3000, true), 800, 605);
         addObject(new SpawnUnitButton(cU3, 2, 1, 3000, true), 700, 605);
-        addObject(new SpawnUnitButton(cU4, 3, 1, 3000, true), 500, 605);
-        addObject(new SpawnUnitButton(cU5, 4, 1, 3000, true), 600, 605);
+        addObject(new SpawnUnitButton(cU4, 3, 1, 3000, true), 600, 605);
+        addObject(new SpawnUnitButton(cU5, 4, 1, 3000, true), 500, 605);
+        
+        addObject(new Cursor(true), 0, 0);
+        addObject(new Cursor(false), 0, 0);
+       
+        //Cursor shows up on top of everything
+        setPaintOrder(FullscreenTransition.class, Cursor.class, UI.class, Effect.class,TowerProjectile.class);
+        
+        addObject(new FullscreenTransition(), 512, 300);
+        
+        Greenfoot.setSpeed(50);
+    }
+    
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public SimulationWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1024, 700, 1, false); 
+        setBackground(bg);
+        
+        addObject(new Offense(false, 500, 100, 0, 1000), 100, 320);
+        addObject(new Offense(true, 500, 100, 0, 1000), 900, 320);
+        
+        //addObject(new CHealer(3), 900, 500);
+        
+        addObject(new Wallet(false), 900, 50);
+        addObject(new Wallet(true), 100, 650);
+        
+        //SpawnUnitButton Parameters
+        //(Name, unitIndex, level, cost, cooldown, canUpgrade, lastButton, visible), x, y)
+        //IMPORTANT: ONE STAGE of ONE UNIT MUST have "true" as the 6th parameter + include 7th param. Other buttons may only have four
+        //This is to cache images of unitStage 1, 2, and 3 for all Units (get rid of freezing)
+        //eg. CU1 level 1 must have the 6th and 7th param
+        addObject(new SpawnUnitButton("SFodder", 0, 1, 3000, true), 100, 55);
+        addObject(new SpawnUnitButton("STank", 1, 1, 3000, true), 200, 55);
+        addObject(new SpawnUnitButton("SWarrior", 2, 1, 3000, true), 300, 55);
+        addObject(new SpawnUnitButton("SRanger", 3, 1, 3000,true), 400, 55);
+        addObject(new SpawnUnitButton("SHealer", 4, 1, 3000, true), 500, 55);
+        
+        addObject(new SpawnUnitButton("CFodder", 0, 1, 3000, true, true, true), 900, 625);
+        addObject(new SpawnUnitButton("CTank", 1, 1, 3000, true), 800, 625);
+        addObject(new SpawnUnitButton("CWarrior", 2, 1, 3000, true), 700, 625);
+        addObject(new SpawnUnitButton("CRanger", 3, 1, 3000, true), 600, 625);
+        addObject(new SpawnUnitButton("CHealer", 4, 1, 3000, true), 500, 625);
         
         addObject(new Cursor(true), 0, 0);
         addObject(new Cursor(false), 0, 0);
